f76c2306956a885079b86a418dd86411ca6c0874
Kilian Agboton <kilian.agboton@gmail.com>
2020-05-12 16:50:59 -0400

    covid-app :: fix test annotations + update poms

--- covid-confirmed-service/src/test/java/io/kagboton/covidtracker/covidconfirmedservice/CovidConfirmedServiceApplicationTests.java
+++ covid-confirmed-service/src/test/java/io/kagboton/covidtracker/covidconfirmedservice/CovidConfirmedServiceApplicationTests.java
@@ -1,13 +1,16 @@
 package io.kagboton.covidtracker.covidconfirmedservice;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class CovidConfirmedServiceApplicationTests {
+public class CovidConfirmedServiceApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
--- covid-global-service/src/test/java/io/kagboton/covidtracker/covidglobalservice/CovidGlobalServiceApplicationTests.java
+++ covid-global-service/src/test/java/io/kagboton/covidtracker/covidglobalservice/CovidGlobalServiceApplicationTests.java
@@ -1,10 +1,13 @@
 package io.kagboton.covidtracker.covidglobalservice;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class CovidGlobalServiceApplicationTests {
+public class CovidGlobalServiceApplicationTests {
 
     @Test
     void contextLoads() {
