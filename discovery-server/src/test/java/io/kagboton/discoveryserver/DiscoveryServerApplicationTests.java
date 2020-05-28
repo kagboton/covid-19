d0a8dae4ad2fed222f56c0e5be7473f46a551137
Kilian Agboton <kilian.agboton@gmail.com>
2020-05-28 09:41:35 -0400

    Changed test dependencies

--- discovery-server/src/test/java/io/kagboton/discoveryserver/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/io/kagboton/discoveryserver/DiscoveryServerApplicationTests.java
@@ -1,9 +1,7 @@
 package io.kagboton.discoveryserver;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 
 @SpringBootTest
 public class DiscoveryServerApplicationTests {
